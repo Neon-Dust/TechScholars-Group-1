271be30fd5d53816f2009fe6cef8fe7c102acb12
Ahmed <ahmedalnoor@Ahmeds-Mac-mini.local>
2021-02-09 13:53:07 -0500

    updated src

--- src/dataClean.cpp
+++ src/dataClean.cpp
@@ -1,9 +1,59 @@
+// libraries
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <cstring>
+#include <regex>
 using namespace std;
 
+void fileCleaner(string &str);
+
+
 int main(){
+  map<string,int> word_count;
+  string read_csv, read_stopwords; // string var for reading csv file and reading stopwords file
+  ofstream cleaned_words; // creates write object for cleaned words file
+  ifstream file_csv, stop_words; // creates read object for csv file and stopwords file
 
 
+  cleaned_words.open("/Users/ahmedalnoor/Documents/TechScholars/TechScholars-Group-1/src/cleaned_words.txt");
+  file_csv.open("/Users/ahmedalnoor/Documents/TechScholars/TechScholars-Group-1/src/jobs.csv"); // take in csv file
+  stop_words.open("/Users/ahmedalnoor/Documents/TechScholars/TechScholars-Group-1/src/stopwords_en_minus_c_r.txt"); // take in csv file
+
+
+  while(getline(file_csv,read_csv)){ // print lines from csv files
+    cout << read_csv << endl;
+    fileCleaner(read_csv);
+  }
+
+
+  while(getline(file_csv,read_stopwords)){ // print lines from csv files
+    cout << read_stopwords << endl;
+  }
 
   return 0;
+
+}
+
+// Function that changes all characters to upper case (to avoid checking for it) and removes all symbols
+void fileCleaner(string &str) {
+
+  //1. Looping through and changing each individual character to upper case.
+  for (int x = 0; x < str.length(); x++) {
+    str[x] = toupper(str[x]);
+  }
+
+  //2. Using regular expressions (C++11) to remove all symbols and non-letters.
+  regex reg{"([A-Z]+)"}; //This regex will search for all upper case letters and omit all symbols.
+  smatch{match}; // This will store the result of the regex.
+  sregex_iterator currentMatch{ str.begin(), str.end(), reg }; //This iterator helps to find all instances of the regex, instead of just one.
+  sregex_iterator lastMatch;
+  while (currentMatch != lastMatch) {
+    match = *currentMatch;
+    cout << match.str() << endl;
+    ++words[match.str()];
+    currentMatch++;
+  }
+
 }
