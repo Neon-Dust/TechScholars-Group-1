edce6709ca0de5f2d617caf1f0f3b1974bbd3d41
Ahmed <ahmednalnoor@gmail.com>
2021-02-10 15:50:24 -0500

    working on erasing

--- src/dataClean.cpp
+++ src/dataClean.cpp
@@ -12,7 +12,7 @@ void fileCleaner(string &str, map<string, int>&);
 
 int main(){
   map<string,int> word_map;
-  map<string, int> stopwords_map;
+  map<string,int> cleaned_word_maps;
   string read_csv, read_stopwords; // string var for reading csv file and reading stopwords file
   ofstream cleaned_words; // creates write object for cleaned words file
   ifstream file_csv, stop_words; // creates read object for csv file and stopwords file
@@ -24,32 +24,60 @@ int main(){
 
 
 
-while(getline(file_csv,read_csv)){ // print lines from csv files
-    fileCleaner(read_csv, word_map);
+  while(getline(file_csv,read_csv)) { // print lines from csv files
+      fileCleaner(read_csv, word_map);
   }
 
-  for (const auto& x : word_map){
-    cout << x.first << ": " << x.second << "\n";
-   }
+  //* Creating a duplicate of the original map of words.
+  cleaned_word_maps.insert(word_map.begin(), word_map.end());
 
+  // cleaned_word_maps.erase("ZOOM");
+  //
+  // for (const auto& x: cleaned_word_maps){
+  //   cout << x.first << ": " << x.second << "\n";
+  // }
+
+
+
+  // for (const auto& x : word_map){
+  //   cout << x.first << ": " << x.second << "\n";
+  // }
 
   while(getline(stop_words,read_stopwords)){ // print lines from csv files
-    //sample_map.insert(pair<int, string>(1, "one"));
     for(int i = 0; i < read_stopwords.length(); i++) {
       read_stopwords[i] = toupper(read_stopwords[i]);
     }
-    //stopwords_map.insert(pair<string,int>(read_stopwords, 0));
-    stopwords_map[read_stopwords] = 0;
-    //++stopwords_map[read_stopwords];
+
+    auto isFound = word_map.find(read_stopwords);
+    if(isFound != word_map.end()){
+      cout << " found ";
+    }
+    // if((word_map.count( read_stopwords)) == 1){
+    //   cleaned_word_maps.erase(read_stopwords);
+    // }
+
+    // cout << read_stopwords << endl;
+    // map<string,int>::iterator it;
+    // it = word_map.find(read_stopwords);
+    // if (it != word_map.end()) {
+    //   cleaned_word_maps.erase (it);
+    // }
+    // if (cleaned_word_maps.find(read_stopwords) == cleaned_word_maps.end()) {
+    //   cleaned_word_maps.erase(read_stopwords);
+    // }
   }
 
-  //for (map<string,int>::iterator iter;iter!=stopwords_map.end();iter++){
-     //cout << (*iter).first << ": " << (*iter).second << "\n";
-  //}
+  cout << cleaned_word_maps.size()<< endl;
+  cout << word_map.size() << endl;
 
-  for (const auto& x : stopwords_map){
-    cout << x.second << ": " << x.first << "\n";
-  }
+  // for (const auto& x: cleaned_word_maps){
+  //   cout << x.first << ": " << x.second << "\n";
+  // }
+
+  // for (const auto& x : word_map){
+  //   cout << x.first << ": " << x.second << "\n";
+  // }
+
 
   file_csv.close();
   stop_words.close();
