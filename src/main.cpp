acd8f86f75c02b1facef50b67aaaffcf480c9804
Tahjour Allman <48869724+Tahjour@users.noreply.github.com>
2021-02-11 12:30:09 -0500

    Cleaned up & Simplified

--- src/main.cpp
+++ src/main.cpp
@@ -1,148 +1,90 @@
-#include <bits/stdc++.h>
-#include <algorithm>
+// Changed this back to only the libraries needed instead of <bits/stdc++>
+// It turns out that actually includes all other known header files in compilation
+// If the focus is efficiency, then it's better to only include the headers we need.
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <map>
-#include <cstring>
+#include <fstream>
 #include <regex>
 
+// This namespace allows us to forget about "std::"
 using namespace std;
 
-// global variables
-string appendto; // stream string buffer; use to append data after cleaning the file
-vector<std::string> filter {}; // vector, dynamic array
+//Function Declarations
+bool sortByValue(const pair<string, int>&, const pair<string, int>&);
+void filterWords(map<string, int>&);
+void fileCleaner(string& str, map<string, int>&);
+void sortAndPrint(map<string, int>); // Called algorithm before
 
-// boolean func - checking if string is inside array
-bool in_array(const std::string &value, const std::vector<std::string> &array)
-{
-    return std::find(array.begin(), array.end(), value) != array.end();
+int main() {
+    ifstream file_csv; // Our input file stream object for jobs.csv file
+    map<string, int> word_map; // our map that will store the words
+    string read_csv;           // string that will store jobs.csv strings line by line
+    file_csv.open("jobs.csv");
+
+    while (getline(file_csv, read_csv)) { fileCleaner(read_csv, word_map); }
+    filterWords(word_map);
+    sortAndPrint(word_map);
+
+    return 0;
 }
 
 // sort values based on value
-bool sortValue(const pair<string, int> &firstValue, const pair<string, int> &secondValue){
+bool sortByValue(const pair<string, int>& firstValue, const pair<string, int>& secondValue) {
     return (firstValue.second > secondValue.second);
 }
 
-void filterWords(){
-  ifstream relWords;
-  string read;
-  // open filter
-  relWords.open("blacklist.txt");
-  while(getline(relWords,read)){
-    // iterate through file and append to dynamic array
-    filter.push_back(read);
-  }
+void filterWords(map<string, int>& word_map) {
+    ifstream relWords;
+    string read;
+    // open filter
+    relWords.open("blacklist.txt");
+    while (getline(relWords, read)) {
+        auto isFound = word_map.find(read);
+        if (isFound != word_map.end()) word_map.erase(read);
+    }
 }
 
-void fileCleaner(string &str, map<string, int>& words) {
+void fileCleaner(string& str, map<string, int>& words) {
 
-  //1. Looping through and changing each individual character to upper case.
-  for (int x = 0; x < str.length(); x++) {
-    str[x] = toupper(str[x]);
-  }
+    //1. Looping through and changing each individual character to upper case.
+    for (int x{ 0 }; x < str.length(); x++) { str[x] = toupper(str[x]); }
 
-  //2. Using regular expressions (C++11) to remove all symbols and non-letters.
-  regex reg{"([A-Z]+)"}; //This regex will search for all upper case letters and omit all symbols.
-  smatch match; // This will store the result of the regex.
-  sregex_iterator currentMatch{ str.begin(), str.end(), reg }; //This iterator helps to find all instances of the regex, instead of just one.
-  sregex_iterator lastMatch;
-  while (currentMatch != lastMatch) {
-    match = *currentMatch;
-    appendto.append(match.str() + ' ');
-    ++words[match.str()];
-    currentMatch++;
-  }
-
+    //2. Using regular expressions (C++11) to remove all symbols and non-letters.
+    regex reg{ "([A-Z]+)" };                                     //This regex will search for all upper case letters and omit all symbols.
+    smatch match;                                                // This will store the result of the regex.
+    sregex_iterator currentMatch{ str.begin(), str.end(), reg }; //This iterator helps to find all instances of the regex, instead of just one.
+    sregex_iterator lastMatch;
+    while (currentMatch != lastMatch) {
+        match = *currentMatch;
+        // appendto.append(match.str() + ' ');
+        ++words[match.str()];
+        currentMatch++;
+    }
 }
 
-void algorithm(map<string, int> wordSort){
+void sortAndPrint(map<string, int> wordSort) {
 
-  // vector pair map, alternative to multimap. Takes values and sorts them
-	vector<pair<string, int>> vectorIndex;
+    // vector pair map, alternative to multimap. Takes values and sorts them
+    vector<pair<string, int>> vectorIndex;
 
+    map<string, int>::iterator iterateWords;
 
-	map<string, int> :: iterator iterateWords;
-
-	for (iterateWords=wordSort.begin(); iterateWords!=wordSort.end(); iterateWords++)
-	{
-		vectorIndex.push_back(make_pair(iterateWords->first, iterateWords->second));
-	}
-
-  // sort algorithm boolean func
-	sort(vectorIndex.begin(), vectorIndex.end(), sortValue);
-
-	// output file
-	ofstream outfile;
-
-  // using ios_base::out write to file (will erase previous data, use ::app instead to preserve data)
-	outfile.open("output_file.csv", ios_base::out);
-
-  // output sorted file into csv file
-	for (int i = 0; i < vectorIndex.size(); i++)
-	{
-		outfile << vectorIndex[i].first << "," << vectorIndex[i].second << endl;
-	}
-
-}
-
-void frequency(string str)
-{
-    map<string, int> wordFrequency;
-
-    string word = "";
-
-    for (int i = 0; i < str.size(); i++) {
-        // delim ' '
-        if (str[i] == ' ') {
-            if (wordFrequency.find(word) == wordFrequency.end()) {
-              // checks if word is in filter, if so does not include
-              if (in_array(word, filter)){
-              }else{
-                // if not in filter then make it a map pair
-                wordFrequency.insert(make_pair(word, 1));
-              }
-                word = "";
-            }
-            else {
-                // add to word
-                wordFrequency[word]++;
-                word = "";
-            }
-        }
-        else{
-          word += str[i];
-        }
+    for (iterateWords = wordSort.begin(); iterateWords != wordSort.end(); iterateWords++) {
+        vectorIndex.push_back(make_pair(iterateWords->first, iterateWords->second));
     }
-    if (wordFrequency.find(word) == wordFrequency.end()){
-        wordFrequency.insert(make_pair(word, 1));
+
+    // sort sortAndPrint boolean func
+    sort(vectorIndex.begin(), vectorIndex.end(), sortByValue);
+
+    // output file
+    ofstream outfile;
+
+    // using ios_base::out write to file (will erase previous data, use ::app instead to preserve data)
+    outfile.open("output_file.csv", ios_base::out);
+
+    // output sorted file into csv file
+    for (int i{ 0 }; i < vectorIndex.size(); i++) {
+        outfile << vectorIndex[i].first << "," << vectorIndex[i].second << endl;
     }
-    else{
-        wordFrequency[word]++;
-    }
-    for (auto& it : wordFrequency){}
-
-    // previously defined alogirthm func in action
-		algorithm(wordFrequency);
-
-
-}
-
-int main()
-{
-  // filtering out the words
-  filterWords();
-  ifstream file_csv;
-  map<string, int> word_map; // map
-  string read_csv; // read
-  file_csv.open("jobs.csv");
-
-  while(getline(file_csv,read_csv)){
-    // information placed inside appendto string
-      fileCleaner(read_csv, word_map);
-    }
-  // iterator; go through the data inside appendto string
-  frequency(appendto);
-
-    return 0;
 }
