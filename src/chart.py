0401bba249b424929f30c99b26c34dd02f06947b
SeanDeveloper <shawn_developer@protonmail.com>
2021-02-15 16:33:29 -0500

    output_file.csv, updated chart.py, added gitignore

--- src/chart.py
+++ src/chart.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import squarify
 
+
 plt.rcParams['toolbar'] = 'None'
 plt.rcParams["figure.figsize"] = (30,15)
 plt.rcParams['font.size'] = 9
 data = pd.read_csv("output_file.csv")
 
-pew = {}
+dicts = {}
 objects = []
 frequency = []
 
@@ -18,9 +19,9 @@ with open("whitelist.txt") as p:
     for word in p.readlines():
         for x in range(data.index.stop):
             if data.loc[x].WORD == word.strip():
-                pew.update({word.strip(): data.loc[x].FREQUENCY})
+                dicts.update({word.strip(): data.loc[x].FREQUENCY})
 
-sortingitems = dict(sorted(pew.items(), key=lambda x: x[1]))
+sortingitems = dict(sorted(dicts.items(), key=lambda x: x[1]))
 for sortitems in sortingitems.items():
 
     objects.append(sortitems[0])
